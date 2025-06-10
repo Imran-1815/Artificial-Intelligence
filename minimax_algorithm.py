0517e57845bf78eaf1839ca92bac4da6a3216cc7
Imran Ahmed Upom <imranghbu15@gmail.com>
2025-06-10 13:32:49 +0600

    update Artificial Intelligence folder

--- minimax_algorithm.py
+++ minimax_algorithm.py
@@ -8,20 +8,80 @@ def print_board(board):
         count+=1
         
     print()
+def is_winner(board,player):
+    for i in range(3):
+        if all(board[i][j]==player for j in range(3)) or all(board[j][i]==player for j in range(3)):
+            return True
+    
+    if all(board[i][i]==player for i in range(3)) or all(board[i][2-i]==player for i in range(3)):
+        return True
+    
+    return False
+
+def is_full(board):
+    for row in board:
+        for cell in row:
+            if cell==" ":
+                return False
+    
+    return True
+
+def Minimax(board,depth,is_maximizing):
+    if is_winner(board,"X"):
+        return 1
+    
+    if is_winner(board,"0"):
+        return -1
+    
+    if is_full(board):
+        return 0
+    
+    if is_maximizing:
+        best_score=-float('inf')
+        
+        for i in range(3):
+            for j in range(3):
+                if board[i][j]==" ":
+                    board[i][j]="X"
+                    
+                    score=Minimax(board,depth+1,False)
+                    
+                    board[i][j]=' '
+                    best_score=max(score,best_score)
+        return best_score
+    
+    else:
+        best_score=float('inf')
+        
+        for i in range(3):
+            for j in range(3):
+                if board[i][j]==" ":
+                    board[i][j]="0"
+                    
+                    score=Minimax(board,depth+1,True)
+                    
+                    board[i][j]=' '
+                    best_score=min(score,best_score)
+        return best_score
 
 
 
-
-
-
-
-
-
-
-
-
-
-
+def best_move(board):
+    best_score=-float('inf')
+    move=None
+    
+    for i in range(3):
+        for j in range(3):
+            if board[i][j]==" ":
+                board[i][j]="X"
+                score=Minimax(board,0,False)
+                board[i][j]=" "
+                
+                if score>best_score:
+                    best_score=score
+                    move=(i,j)
+    
+    return move
 
 
 
@@ -42,5 +102,51 @@ while True:
         print("cell not emplty. retry")
         continue
     board[row][col]="0"
+    
+    if is_winner(board,"0"):
+        print_board(board)
+        print('you win')
+        break
+    
+    if is_full(board):
+        print_board(board)
+        print("Draw")
+        break
+    
+    #Ai_move
+    
+    ai_move=best_move(board)
+    board[ai_move[0]][ai_move[1]]="X"
+    
+    print("Ai played")
+    print_board(board)
+    
+    if is_winner(board,"X"):
+        print("Ai wins")
+        break
+    
+    if is_full(board):
+        print("Draw")
+        break
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     
     
